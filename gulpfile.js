9c0a49e543cf873fd5833a5d0d38c86be314fc14
Uillian Freitas <freitasuillian@gmail.com>
2026-02-10 15:07:19 -0300

    estrutura inicial do projeto Harley Stream

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp =require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
+
+function styles(){
+    return gulp.src('./src/style/main.scss')
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(cleanCSS())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./dist/css'))
+}
+
+function watchFiles(){
+    gulp.watch('.src/styles/**/*.scss', styles);
+}
+
+exports.styles = styles;
+exports.watch = watchFiles;
